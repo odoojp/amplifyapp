855891f9b364e301503d02279b23e9ac497069da
川口弘之 <kawaguchihiroyuki@kawaguchihiroyukinoMacBook-Pro.local>
2022-04-24 21:10:20 +0900

    addedauthv2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
+// APPA
 function App() {
   return (
     <div className="App">
